beffa2601073af6483e09d2a108dcf9ee43bf644
P-marashi <pouyamarashii@gmail.com>
2024-05-01 14:34:17 +0330

    delete manifest and update gitignore

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,44 +1,24 @@
-#!/usr/bin/env python
 import os
 import shutil
-import yaml
-
-MANIFEST = 'manifest.yml'
 
 
-def delete_resources_for_disabled_features():
-    """ Deleting unnecessary files after 
-        cookiecutter files generation """
-    with open(MANIFEST) as manifest_file:
-        manifest = yaml.load(manifest_file, Loader=yaml.FullLoader)
-        for feature in  manifest['features']:
-            if feature.get('enabled'):
-                if feature['enabled'] == "n":
-                    for resource in feature['resources']:
-                        delete_resource(resource)
-            elif feature.get('technology'):
-                if feature['technology'] == "BasicHTML":
-                    for resource in feature['resources']:
-                        delete_resource(resource)
-                else:
-                    for resource in feature['othersources']:
-                        delete_resource(resource)
-    print("[+] Cleanup completed, removing manifest")
-    delete_resource(MANIFEST)
+license = "{{cookiecutter.license}}"
+jwt = "{{cookiecutter.use_jwt}}"
+project_slug = "{{cookiecutter.project_slug}}"
 
 
 def delete_resource(resource):
-    """ helper function: removing file
-        or directories for delete 
-        resources for disabled features 
-    """
     if os.path.isfile(resource):
-        print(f"[*] Removing file: {resource}")
+        print(f"removing file: {resource}")
         os.remove(resource)
     elif os.path.isdir(resource):
-        print(f"[*] Removing directory: {resource}")
+        print(f"removing directory: {resource}")
         shutil.rmtree(resource)
 
 
-if __name__ == '__main__':
-    delete_resources_for_disabled_features()
+if license == "None":
+    delete_resource("LICENSE")
+if jwt == "n":
+    delete_resource(f"{project_slug}/authentication/")
+    delete_resource(f"{project_slug}/users/")
+
